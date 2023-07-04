95af7b020b20ccff1bcec5de640225a0525ea759
mehulklh <106767917+mehulklh@users.noreply.github.com>
2023-07-04 21:29:14 +0530

    Updated box breathing

--- src/box_breathing/script.js
+++ src/box_breathing/script.js
@@ -1,5 +1,24 @@
 /** @format */
 
+function sleep(seconds) {
+	return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+async function startCountdown() {
+	var countDowner = document.getElementById("center");
+	await sleep(2);
+	countDowner.innerHTML = "<div id='countdown'>3</div>";
+	await sleep(1);
+	countDowner.innerHTML = "<div id='countdown'>2</div>";
+	await sleep(1);
+	countDowner.innerHTML = "<div id='countdown'>1</div>";
+	await sleep(1);
+	countDowner.innerHTML = "<div id='countdown'>GO!</div>";
+	await sleep(1);
+    countDowner.innerHTML = "<img src='square4.svg'>";
+    await breathe();
+}
+
 const leftDiv = document.getElementById("left");
 
 function displayText(text, duration) {
@@ -9,18 +28,18 @@ function displayText(text, duration) {
 	}, duration);
 }
 
-function breathe() {
+async function breathe() {
 	displayText("Breathe In... Feel the air enter your lungs", 4000);
-	setTimeout(() => {
+	setTimeout(async () => {
 		displayText("Hold your breath...", 4000);
-		setTimeout(() => {
-			displayText("Exhale through you mouth", 4000);
-			setTimeout(() => {
-				displayText("Inhale", 4000);
-				setTimeout(breathe, 4000);
+		setTimeout(async () => {
+			displayText("Exhale through your mouth", 4000);
+			setTimeout(async () => {
+				displayText("Hold the lungs in an empty state", 4000);
+				await breathe(); 
 			}, 4000);
 		}, 4000);
 	}, 4000);
 }
 
-breathe();
+
